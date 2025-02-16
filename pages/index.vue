ed43256de30ad600fde49be674c7855894ffc8b6
hisaro <worldwidehisa@gmail.com>
2025-02-16 11:02:56 +0900

    add typography

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <h1>Hello World Wide Hisa</h1>
+  <!-- typographyプラグインのスタイルを適用する -->
+  <div class="prose mx-auto">
+    <h1>Hello World Wide Hisa 2</h1>
+    <p>文章のサンプル</p>
   </div>
 </template>
