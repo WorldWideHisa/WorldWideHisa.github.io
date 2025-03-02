1161cfd41151e7c3be0990eb1c75ce7fde0d502e
hisaro <worldwidehisa@gmail.com>
2025-03-02 23:03:26 +0900

    デザイン周り調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <!-- typographyプラグインのスタイルを適用する -->
-  <div class="prose mx-auto">
-    <h1>Hello World Wide Hisa 2</h1>
-    <p>文章のサンプル</p>
+  <div class="bg-gray-50 min-h-screen flex flex-col">
+    <!-- メイン -->
+    <div class="pt-32 pb-10 text-center">
+      <h1 class="text-4xl font-bold mb-4">Hello World</h1>
+      <p class="text-lg">ここは説明文など</p>
+
+      <!-- 疑似的にコンテンツが長い状態を再現 -->
+      <div class="h-[1200px]"></div>
+    </div>
+    <!-- フッター -->
+    <footer class="bg-gray-700 py-4 border-t border-gray-200">
+      <nav class="flex justify-center space-x-8">
+        <nuxt-link to="/about" class="text-gray-300 hover:text-white">About</nuxt-link>
+        <nuxt-link to="/articles" class="text-gray-300 hover:text-white">Articles</nuxt-link>
+      </nav>
+      <p class="text-center text-sm text-gray-500 mt-4">©2025 World Wide Hisa</p>
+    </footer>
   </div>
 </template>
