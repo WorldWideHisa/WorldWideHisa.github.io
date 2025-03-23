cf8c89949962c0a3b980c2fd956882a30cc28387
hisaro <worldwidehisa@gmail.com>
2025-03-23 20:03:07 +0900

    layouts導入でデザインなどを流用できるように

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-900">
+    <main class="flex-grow">
+      <slot />
+    </main>
+    <footer class="bg-gray-700 py-4 border-t border-gray-200">
+      <nav class="flex justify-center space-x-8">
+        <NuxtLink to="/about" class="text-gray-300 hover:text-white">About</NuxtLink>
+        <NuxtLink to="/articles" class="text-gray-300 hover:text-white">Articles</NuxtLink>
+      </nav>
+      <p class="text-center text-sm text-gray-500 mt-4">©2025 World Wide Hisa</p>
+    </footer>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-50 min-h-screen flex flex-col">
+  <div>
     <!-- メイン -->
     <div class="pt-32 pb-10 text-center">
       <h1 class="text-4xl font-bold mb-4">Hello World</h1>
@@ -8,13 +8,5 @@
       <!-- 疑似的にコンテンツが長い状態を再現 -->
       <div class="h-[1200px]"></div>
     </div>
-    <!-- フッター -->
-    <footer class="bg-gray-700 py-4 border-t border-gray-200">
-      <nav class="flex justify-center space-x-8">
-        <nuxt-link to="/about" class="text-gray-300 hover:text-white">About</nuxt-link>
-        <nuxt-link to="/articles" class="text-gray-300 hover:text-white">Articles</nuxt-link>
-      </nav>
-      <p class="text-center text-sm text-gray-500 mt-4">©2025 World Wide Hisa</p>
-    </footer>
   </div>
 </template>
