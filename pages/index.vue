79fd6744b9f87783bf1a5b9e634693dae340788b
hisaro <worldwidehisa@gmail.com>
2025-04-27 18:16:03 +0900

    文言修正

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div>
     <!-- メイン -->
     <div class="pt-32 pb-10 text-center">
-      <h1 class="text-4xl font-bold mb-4">Hello World</h1>
-      <p class="text-lg">ここは説明文など</p>
+      <h1 class="text-4xl font-bold mb-4">World Wide Hisa</h1>
+      <p class="text-lg">WEB技術や趣味のことをひっそりと書いておくところです</p>
     </div>
     <!-- 最新記事リスト -->
     <div class="max-w-4xl mx-auto px-4 pb-16">
@@ -35,7 +35,7 @@ interface Article {
 const { data: latestArticles } = await useAsyncData("latestArticles", () =>
   queryContent("/articles")
     .sort({ date: -1 }) // 日付の降順でソート
-    .limit(5) // 5件に制限
+    .limit(5) // 3件に制限
     .find()
 );
 
