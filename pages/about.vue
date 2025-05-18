bdb263a9d3922ede788319096f5afde9687e47a1
hisaro <worldwidehisa@gmail.com>
2025-05-18 14:46:20 +0900

    about調整

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@
     <div class="mb-10 text-center">
       <h1 class="text-4xl font-bold mb-4">About Me</h1>
       <p class="text-lg text-gray-700">
-        このブログの管理人についての紹介ページです
+        よおこそ
       </p>
     </div>
 
@@ -12,18 +12,17 @@
     <div class="mb-10">
       <h2 class="text-2xl font-semibold mb-4">プロフィール</h2>
       <div class="mt-6">
-        <p class="text-xl font-medium mb-4">Hisa</p>
         <div class="space-y-4 text-gray-700">
           <p class="flex items-center">
             <span class="font-medium w-32">技術アカウント:</span>
-            <a href="https://twitter.com/xxxハンドルネーム" class="text-blue-600 hover:underline">
-              @xxxハンドルネーム
+            <a href="https://twitter.com/" class="text-blue-600 hover:underline">
+              これからたぶん作る
             </a>
           </p>
           <p class="flex items-center">
             <span class="font-medium w-32">趣味アカウント:</span>
-            <a href="#" class="text-blue-600 hover:underline">
-              @xxxハンドルネーム
+            <a href="https://x.com/hisaro7sec" class="text-blue-600 hover:underline">
+              ヒサロウ/@hisaro7sec
             </a>
           </p>
         </div>
@@ -31,19 +30,37 @@
     </div>
 
     <!-- ブログについてのセクション -->
-    <div>
+    <div class="mb-10">
       <h2 class="text-2xl font-semibold mb-4">このブログについて</h2>
       <div class="mt-6 space-y-4 text-gray-700">
         <p>
-          このブログでは、日常の生活や技術的な学びを趣味として書き留めていきます。
+          このブログでは、趣味の話や日常、稀に技術のことを書き留めます。<br>
+          あまり堅苦しいことは書かないかな～と思います。特に自慢できる経歴とかもないし。
         </p>
+      </div>
+    </div>
+    <div>
+      <h2 class="text-2xl font-semibold mb-4">本業と趣味</h2>
+      <div class="mt-6 space-y-4 text-gray-700">
         <p>
-          Web開発、プログラミング、新しい技術の探求など、技術的なトピックを中心に、
-          時には日常の発見や趣味の話題も交えながら投稿していきます。
+          本業は会社でWebアプリケーション開発・運用をしています。<br>
+          JavaScript、TypeScriptをよく使います。<br>
+          業務ではクラウドからバックエンド、フロントエンドまで必要に駆られれば割と広く触ります。<br>
+          小さくものが出来上がることが好きなので、このブログも自作しています。<br>
+          「Nuxt Content」というCMSライクな仕組みを提供する、Nuxt3向けのモジュールを利用して作成していてGitHubへ公開しています。
         </p>
+        <a href="https://github.com/WorldWideHisa/WorldWideHisa.github.io" class="text-blue-600 hover:underline">
+          https://github.com/WorldWideHisa/WorldWideHisa.github.io
+        </a>
         <p>
-          特にNuxt、TypeScript、TailwindCSSなどのモダンなWeb技術に関する内容が多くなる予定です。
+          趣味は家に居ることが好きで、主にPCゲームを友人とやっていることが多かったですが、最近は居間でSwitchなどで遊ぶことも多いです。<br>
+          ルーツはニコニコ動画、LoL、FPS、東方Project、beatmania、麻雀...などなどです。ルーツが近い人はなんとなく人柄がわかるでしょう。<br>
+          他にはPCデスク周りや部屋をきれいにしたり、自作キーボード、カスタムキーボードなどに興味があります。<br>
+          主に東方ですが、昔はイラストを頑張ろうとしていたこともあり、pixivに過去絵を置いておくのでよかったら見てください。<br>
         </p>
+        <a href="https://www.pixiv.net/users/2097770" class="text-blue-600 hover:underline">
+          https://www.pixiv.net/users/2097770
+        </a>
       </div>
     </div>
   </div>
